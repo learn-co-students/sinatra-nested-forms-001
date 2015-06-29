0d06ebfd4cfbdec80ad009646fc3414a3a6ffb13
Lia Skalkos <hello@liaskalkos.net>
2015-06-29 16:42:32 -0400

    Add front end

--- app.rb
+++ app.rb
@@ -6,7 +6,9 @@ module FormsLab
     # code other routes/actions here
 
     get '/' do
-      "Welcome to the Nested Forms Lab! let's navigate to the '/new'"
+      # "Welcome to the Nested Forms Lab! let's navigate to the '/new'"
+
+      erb :'pirates/index'
     end
 
     get '/new' do
